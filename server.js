60027dea8daebe1766570cd3ef2f372c74e60dea
unknown <sandu@ucdavis.edu>
2022-11-14 22:48:12 -0800

    install mongoose and database connection made

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+const assert = require("assert");
 const express = require("express");
-const PORT = process.env.PORT || 8080;
+const mongoose = require("mongoose");
 
 const app = express();
 
+const PORT = process.env.PORT || 8080;
 
+const dbName = "reactorERP";
+const url = "mongodb://localhost:27017/"+ dbName;
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(url);
+    console.log(`Connection made on ${url}`);
+};
 
 app.listen(PORT, console.log(`App is listening on port ${PORT}`));
